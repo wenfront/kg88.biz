66299c5f8c39a9f808d950a499b23f95fead809c
webfront <webfront65@gmail.com>
2024-06-29 14:53:58 +0700

    update commit

---
+++ js/right.js
@@ -0,0 +1,124 @@
+document.querySelector("#rigth").innerHTML = `
+<div class=" w-[100%] md:border-l border-gray-600 md:pl-4">
+            <h3 class="h3">BÀI VIẾT MỚI</h3>
+            <div class="flex flex-col pt-8">
+              <article class="flex gap-2 border-b border-gray-300 pb-2">
+                <div class="md:w-[150px] w-[70px]">
+                  <img src="../src/r1.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth1.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                  >Hướng Dẫn Cách Nuôi Dàn Đề 64 Số Dành Cho Tân Binh Mới</a
+                >
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div class="md:w-[150px] w-[70px]">
+                  <img src="../src/r2.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth2.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                >
+                  Bật Mí Mẹo Chơi Bát Châu Báu Chắc Thắng Rinh Thưởng Khủng</a
+                >
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div class="md:w-[150px] w-[70px]">
+                  <img src="../src/r3.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth3.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                >
+                  Hé Lộ Mẹo Chơi Xúc Xắc Màu May Mắn Hiệu Quả Nhất 2024</a
+                >
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div
+                  class="md:w-[150px] w-[70px] hover:text-gray-900 text-[15px]"
+                >
+                  <img src="../src/r4.png" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth4.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                >
+                  Tổng hợp mẹo chơi Baccarat hiệu quả rinh thưởng khủng 2024</a
+                >
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div
+                  class="md:w-[150px] w-[70px] hover:text-gray-900 text-[15px]"
+                >
+                  <img src="../src/r5.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth5.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                >
+                  Mẹo Chơi Siêu Bò Tót B – Game Nổ Hũ Đình Đám 2024</a
+                >
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div
+                  class="md:w-[150px] w-[70px] hover:text-gray-900 text-[15px]"
+                >
+                  <img src="../src/r6.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth6.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                >
+                  Kèo Châu Á Là Gì? Kinh Nghiệm Vào Kèo Cho Mọi Người
+                </a>
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div
+                  class="md:w-[150px] w-[70px] hover:text-gray-900 text-[15px]"
+                >
+                  <img src="../src/r7.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth7.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                >
+                  Mẹo Chơi Cá Cược Thể Thao Lụm Tiền Thưởng Khủng</a
+                >
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div
+                  class="md:w-[150px] w-[70px] hover:text-gray-900 text-[15px]"
+                >
+                  <img src="../src/r8.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth8.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                  >Hướng Dẫn Cách Chơi Bạch Thủ Lô Hốt Bạc Cho Game Thủ</a
+                >
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div class="md:w-[150px] w-[70px]">
+                  <img src="../src/r9.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth9.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                >
+                  Bạch Thủ Lô Là Gì? Kinh Nghiệm Đánh Chuẩn Xác Nhất</a
+                >
+              </article>
+              <article class="flex gap-2 border-b border-gray-300 py-2">
+                <div class="md:w-[150px] w-[70px]">
+                  <img src="../src/r10.jfif" alt="" class="w-full" />
+                </div>
+                <a
+                  href="../components/rigth10.html"
+                  class="text-red-700 hover:text-gray-900 text-[15px]"
+                  >Hướng Dẫn Cách Lên Dàn Đề 64 Số Hiệu Quả Thắng Lớn</a
+                >
+              </article>
+            </div>
+          </div>
+`;
